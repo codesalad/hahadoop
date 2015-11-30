d8845b363d3be7367dc3db6619c80c9799594ed6
cloudera <cloudera@quickstart.cloudera>
2015-11-30 14:16:26 -0800

    Added more counters, setup, cleanup, refactored

--- java/src/WordCount.java
+++ java/src/WordCount.java
@@ -9,6 +9,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -17,95 +18,122 @@ import org.apache.hadoop.io.Text;
 import org.apache.log4j.Logger;
 
 enum Records {
+	UNIQUE_TERMS,
 	DISTINCT_TERMS,
 	COUNT_TS,
-	TERMS_LT5;
+	TERMS_LT5,
+	FILESTOTAL;
 };
 
 public class WordCount extends Configured implements Tool {
 
-  private static final Logger LOG = Logger.getLogger(WordCount.class);
+	private static final Logger LOG = Logger.getLogger(WordCount.class);
 
-  public static void main(String[] args) throws Exception {
-    int res = ToolRunner.run(new WordCount(), args);
-    System.exit(res);
-  }
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(new WordCount(), args);
+		System.exit(res);
+	}
 
-  public int run(String[] args) throws Exception {
-    Job job = Job.getInstance(getConf(), "wordcount");
-    job.setJarByClass(this.getClass());
-    // Use TextInputFormat, the default unless job.setInputFormatClass is used
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    job.setMapperClass(Map.class);
-    job.setCombinerClass(Combine.class);
-    job.setReducerClass(Reduce.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    return job.waitForCompletion(true) ? 0 : 1;
-  }
+	public int run(String[] args) throws Exception {
+		Job job = Job.getInstance(getConf(), "wordcount");
+		job.setJarByClass(this.getClass());
+		// Use TextInputFormat, the default unless job.setInputFormatClass is used
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setMapperClass(Map.class);
+		job.setCombinerClass(Combine.class);
+		job.setReducerClass(Reduce.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
 
-  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-    private long numRecords = 0;    
-    private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");
-    
-    private static final Pattern FILTER = Pattern.compile("([A-Z][a-z]+)|([a-z]\\w+)");
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		private Text filename = new Text();
+		private long numRecords = 0;    
+		private static final Pattern WORD_BOUNDARY = Pattern.compile("\\s*\\b\\s*");
 
-    public void map(LongWritable offset, Text lineText, Context context)
-        throws IOException, InterruptedException {
-      String line = lineText.toString().toLowerCase();
-      Text currentWord = new Text();
-      for (String word : WORD_BOUNDARY.split(line)) {
-        if (word.isEmpty()) {
-            continue;
-        } 
-        
-        // Count number of words that start with T/t
-        if (word.matches("^[Tt].*+"))
-        	context.getCounter(Records.COUNT_TS).increment(1);
-        
-        Matcher nospecials = FILTER.matcher(word);
-        if (nospecials.find()) {
-        	currentWord = new Text(word);
-            context.write(currentWord,one);
-        }
-      }
-    }
-  }
+		private static final Pattern FILTER = Pattern.compile("([A-Z][a-z]+)|([a-z]\\w+)");
 
-  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-    @Override
-    public void reduce(Text word, Iterable<IntWritable> counts, Context context)
-        throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable count : counts) {
-        sum += count.get();
-      }
-      
-      //Count distinct elements
-		if (sum == 1)
-			context.getCounter(Records.DISTINCT_TERMS).increment(1);
-      
-      // Count words with sum < 5
-		if (sum < 5)
-			context.getCounter(Records.TERMS_LT5).increment(1);
-      
-      context.write(word, new IntWritable(sum));
-    }
-  }
-  
-  public static class Combine extends Reducer<Text, IntWritable, Text, IntWritable> {
-	    @Override
-	    public void reduce(Text word, Iterable<IntWritable> counts, Context context)
-	        throws IOException, InterruptedException {
-	      int sum = 0;
-	      for (IntWritable count : counts) {
-	        sum += count.get();
-	      }
-	      
-	      context.write(word, new IntWritable(sum));
-	    }
-	  }
+		public void map(LongWritable offset, Text lineText, Context context)
+				throws IOException, InterruptedException {
+			String line = lineText.toString().toLowerCase();
+			Text currentWord = new Text();
+			for (String word : WORD_BOUNDARY.split(line)) {
+				if (word.isEmpty()) {
+					continue;
+				} 
+
+				// Get filename the word is in.
+				String filenameStr = ((FileSplit) context.getInputSplit()).getPath().getName();
+				filename = new Text(filenameStr);
+				context.write(filename, one);
+				
+				// Count number of words that start with T/t
+				if (word.matches("^[Tt].*$"))
+					context.getCounter(Records.COUNT_TS).increment(1);
+
+				// Filter out special signs
+				Matcher nospecials = FILTER.matcher(word);
+				if (nospecials.find()) {
+					currentWord = new Text(word);
+					context.write(currentWord,one);
+				}
+			}
+		}
+	}
+
+	public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+		
+		@Override
+		public void setup(Context context) throws IOException {
+			// Might be handy for later.
+		}
+		
+		@Override
+		public void reduce(Text word, Iterable<IntWritable> counts, Context context)
+				throws IOException, InterruptedException {
+			
+			if (!word.toString().matches("^.+txt$")) {
+				int sum = 0;
+				for (IntWritable count : counts) {
+					sum += count.get();
+				}
+				
+				//Count distinct elements
+				if (sum == 1) 
+					context.getCounter(Records.DISTINCT_TERMS).increment(1);
+				
+				// Count words with sum < 5
+				if (sum < 5)
+					context.getCounter(Records.TERMS_LT5).increment(1);
+
+				context.getCounter(Records.UNIQUE_TERMS).increment(1);
+				context.write(word, new IntWritable(sum));	
+			} else {
+				// Count distinct files
+				context.getCounter(Records.FILESTOTAL).increment(1);
+			}
+		}
+		
+		@Override
+		public void cleanup(Context context) throws IOException, InterruptedException {
+			// Might be handy for later.
+		}
+	}
+
+	public static class Combine extends Reducer<Text, IntWritable, Text, IntWritable> {
+		@Override
+		public void reduce(Text word, Iterable<IntWritable> counts, Context context)
+				throws IOException, InterruptedException {
+			
+			int sum = 0;
+			for (IntWritable count : counts) {
+				sum += count.get();
+			}
+			context.write(word, new IntWritable(sum));
+		}
+	}
 }

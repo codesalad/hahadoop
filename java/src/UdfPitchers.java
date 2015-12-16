f4022abf9ecd428b70c8ad677914d136a8c2603b
codesalad <nguyen094@gmail.com>
2015-12-16 10:04:06 -0800

    query5 done

--- java/src/UdfPitchers.java
+++ java/src/UdfPitchers.java
@@ -11,29 +11,62 @@ import org.apache.pig.impl.util.WrappedIOException;
 
 public class UdfPitchers extends FilterFunc {
 
+	@SuppressWarnings("unchecked")
 	@Override
+	/**
+	 * Input: bag containing player tuple(s)
+	 * simplified input: {(playertuple), (playertuple)}
+	 * 
+	 * Output: player meets condition: 
+	 * 	(is pitcher) and (strikeouts/games > avg strikeouts/games) 
+	 * 
+	 * Some players have more than 1 records
+	 * (player, {(player,team,{positions},map[], avg), (player,team,{positions},map[], avg)}
+	 */
     public Boolean exec(Tuple input) throws IOException {
         if (input == null || input.size() == 0)
             return null;
         try {
-            Object values = input.get(0);
-//            if (values instanceof DataBag)
-//                return ((DataBag)values).size() == 0;
-//            else if (values instanceof Map)
-//                return ((Map)values).size() == 0;
-//            else{
-//                throw new IOException("Cannot test a " +
-//                    DataType.findTypeName(values) + " for emptiness.");
-//            }
-            DataBag t = (DataBag) values;
+            DataBag t = (DataBag) input.get(0) ;
             Iterator<Tuple> tupleIterator = t.iterator();
+            
+            boolean isPitcher = false;
+            float avgx = Float.MAX_VALUE;
+            int strikeouts = 0;
+            int games = 0;
+            
+            // Iterates through player tuples (some have more than 1)
             for (int i = 0; tupleIterator.hasNext(); i++) {
-            	if (i == 0) {
-            		String text = (String) tupleIterator.next().get(0);
-            		if (text.equals("Jarrod Saltalamacchia")) 
-            			return true;
+            	Tuple playerTuple = (Tuple) tupleIterator.next();
+            	
+            	if (playerTuple.size() == 0) continue;
+            	if (playerTuple.get(2) == null 
+        			|| playerTuple.get(3) == null 
+        			|| playerTuple.get(4) == null) {
+        			return false;
             	}
+            	
+            	DataBag positions = (DataBag) playerTuple.get(2);
+            	Map<String, Object> bat = (Map<String, Object>) playerTuple.get(3);
+            	avgx = (float) playerTuple.get(4);
+            	
+            	Iterator<Tuple> positionsIt = positions.iterator();
+            	while (positionsIt.hasNext() && !isPitcher) {
+            		String player_position = (String) positionsIt.next().get(0);
+            		if (player_position.equals("Pitcher"))
+            			isPitcher = true;
+            	}
+            	
+            	if (bat.get("strikeouts") != null)
+            		strikeouts += Integer.parseInt(bat.get("strikeouts").toString());
+            	if (bat.get("games") != null)
+            		games += Integer.parseInt(bat.get("games").toString());
             }
+            
+            float x = (float) strikeouts / games;
+            if (isPitcher && x >= avgx)
+            	return true;
+            
         } catch (ExecException ee) {
             ee.printStackTrace(); 
         }
